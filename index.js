b1c6808646ac7cc53918fb163a300165629337e7
rafaelgamboaa <48702411+rafaelgamboaa@users.noreply.github.com>
2022-08-06 22:33:06 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send( {message: 'app funcionando en el puerto 3000'} )
+    response.send("La pagina de inicio")
 })
 
 app.listen(3000, console.log('app funcionando en el puerto 3000'))
