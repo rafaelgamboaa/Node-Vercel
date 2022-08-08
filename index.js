e1b48a77dfc6d66bf9e4a9f7a3586fe48371b92d
rafaelgamboaa <48702411+rafaelgamboaa@users.noreply.github.com>
2022-08-07 22:15:03 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.get('/', (request, response) => {
-    response.send("La pagina de inicio")
-})
+//Base de Datos
+const uri = 'mongodb+srv://rg:rg@almuerzi.8x8rbsn.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}
+    ).then(() => console.log('DB Connected'))
+     .catch(e => console.log(e))
+
+//
+
+
+
+app.get('/', (req, res) => { 
+    res.send('La pagina de inicio')
+ })
 
 app.listen(3000, console.log('app funcionando en el puerto 3000'))
