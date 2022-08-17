b94e912ba6b69fe2fc495ddbeb75d21c3bb9e668
Irwing Naranjo <khalisser@gmail.com>
2022-08-17 00:33:43 -0400

    REFACTOR improving the syntax

--- api/index.js
+++ api/index.js
@@ -6,23 +6,26 @@ const meals = require('./routes/meals')
 const orders = require('./routes/orders')
 const app = express()
 
-// USE ENVIRONMENT VARIABLES
+// Use environment variables
 const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://fcc-mongodb:mongodb@cluster0.mpo46.mongodb.net/?retryWrites=true&w=majority'
 
 // CORS
 app.use(cors())
 
-//BODY-PARSER
+// Use body parse
 app.use(bodyParser.json())
 
-//DB
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}
-    ).then(() => console.log('DB Connected'))
-     .catch(e => console.log(e))
-//
+// Connection Database
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'))
+    .catch(e => console.log(e))
 
+// Routes
 app.use('/api/meals', meals)
 app.use('/api/orders', orders)
 
-app.listen(PORT, console.log(`app funcionando en el puerto ${PORT}`))
+app.listen(PORT, console.log(`App funcionando en el puerto ${PORT}`))
