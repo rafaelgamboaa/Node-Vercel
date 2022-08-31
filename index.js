f50816909f18b614993119ff852a5bb0d6019273
rafaelgamboaa <48702411+rafaelgamboaa@users.noreply.github.com>
2022-08-31 16:30:54 -0400

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const meals = require('./api/routes/meals')
+const orders = require('./api/routes/orders')
+const app = express()
+
+// Use environment variables
+const PORT = process.env.PORT || 3000
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://rg:rg@rafa.eqszqcd.mongodb.net/?retryWrites=true&w=majority'
+
+// CORS
+app.use(cors())
+
+// Use body parse
+app.use(bodyParser.json())
+
+// Connection Database
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'))
+    .catch(error => console.log(error))
+
+// Routes
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
+
+// TODO: Add 404 route
+// TODO: Add swagger docs
+
+app.listen(PORT, console.log(`App funcionando en el puerto ${PORT}`))
